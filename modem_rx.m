935c73ef2a6d5beb1208eb9be5b8cc5e3a5adbf6
lila <lsmith2@olin.edu>
2022-05-04 16:05:50 -0400

    Fixed bandwidth on lowpass filter bug.

--- modem_rx.m
+++ modem_rx.m
@@ -20,14 +20,14 @@ y_c = c .* y_t';
 
 % Use a lowpass filter to filter high frequencies created with cosine
 % We convolve because we want to multiply in frequency domain
-W = f_c/Fs;
+W = 0.5*f_c;
 h_lowpass = (W/pi)*sinc(W/pi*t);
 y_l = conv(y_c, h_lowpass);
 
 % Find average highs and lows per symbol period for message length
 x_d = zeros([msg_length*8, 1]);
 for i=1:length(x_d)
-    average = mean(y_c((i-1)*100+1:i*100));
+    average = mean(y_l((i-1)*100+1:i*100));
     x_d(i) = average > 0; % Write a 1 if co
 end
 
